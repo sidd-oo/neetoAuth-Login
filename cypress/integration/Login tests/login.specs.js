d9780144fe9358ba3e06a52b4065a598708405d8
sidd_oo <sahoosiddharth3@gmail.com>
2021-05-16 21:46:53 +0530

    modified login.spec.js - edited the indentation, removed loginSubmit(), renamed some tests

--- cypress/integration/Login tests/login.specs.js
+++ cypress/integration/Login tests/login.specs.js
@@ -6,66 +6,62 @@
    beforeEach(() => {
      cy.viewport(1280,720);
      cy.visit('https://spinkart.neetoauth.net')
-     cy.fixture("credentials").then((user)=>{
+     cy.fixture("credentials").then( user => {
        userDetails = user;
      })
   });
     
   it("Login test with correct email and correct password", () => {
       cy.login(userDetails.correct.email,userDetails.correct.password);
-      cy.loginSubmit();
   });
 
   it("Login test with correct email and wrong password", () => {
       cy.login(userDetails.correct.email,userDetails.incorrect.password);
-      cy.loginSubmit();
       cy.msgPrompt('Something went wrong.');
   });
 
-  it("Login test with wrong email and correct password", () => {
+  it("Login test with wrong email", () => {
       cy.login(userDetails.incorrect.email,userDetails.correct.password);
-      cy.loginSubmit();
       cy.msgPrompt('Something went wrong.');
   });
 
   it("Login test with wrong email and wrong password", () => {
       cy.login(userDetails.incorrect.email,userDetails.incorrect.password);
-      cy.loginSubmit();
       cy.msgPrompt('Something went wrong.');
   });
 
-  it("Login test with empty email and empty password", () => {
+  it("Login test with empty email field and empty password field", () => {
       cy.get('[data-cy="login-email-text-field"]').and(($input) => {
             expect($input).to.have.value('')
       })
       cy.get('[data-cy="login-password-text-field"]').and(($input) => {
             expect($input).to.have.value('')
       })
-      cy.loginSubmit();
-      cy.location().should((loc) => {
+
+      cy.location().should(loc => {
           expect(loc.toString()).to.eq('https://spinkart.neetoauth.net/login')
       })
 
     });
 
-    it("Login test with empty email and correct password", () => {
+    it("Login test with empty email field and filled password field", () => {
       cy.get('[data-cy="login-email-text-field"]').and(($input) => {
             expect($input).to.have.value('')
       })
       cy.get('[data-cy="login-password-text-field"]').type(userDetails.correct.password);
-      cy.loginSubmit();
+
       cy.location().should((loc) => {
           expect(loc.toString()).to.eq('https://spinkart.neetoauth.net/login')
       })
     });
 
-    it("Login test with correct email and empty password", () => {
+    it("Login test with filled email field and empty password field", () => {
       cy.get('[data-cy="login-email-text-field"]').type(userDetails.correct.email);
       cy.get('[data-cy="login-password-text-field"]').and(($input) => {
             expect($input).to.have.value('')
       })
-      cy.loginSubmit();
-      cy.location().should((loc) => {
+
+      cy.location().should(loc => {
           expect(loc.toString()).to.eq('https://spinkart.neetoauth.net/login')
       })
     });
