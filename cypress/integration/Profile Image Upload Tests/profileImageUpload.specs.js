a8e556e5c00fb593e74dfc79b1ef5f55c42a3659
sidd_oo <sahoosiddharth3@gmail.com>
2021-05-17 10:15:56 +0530

    Modified the selector for Country, Timezone select dropdown

--- cypress/integration/Profile Image Upload Tests/profileImageUpload.specs.js
+++ cypress/integration/Profile Image Upload Tests/profileImageUpload.specs.js
@@ -54,10 +54,10 @@ describe("NeetoAuth Profile Image upload functionality", () => {
     cy.contains('Profile image successfully removed!').should('be.visible');
   })
 
-  it("Updating first name, Change Country, Select timezone, Change date format",() => {
+  it.only("Updating first name, Change Country, Select timezone, Change date format",() => {
     cy.get('[data-cy=profile-first-name-text-field]').clear().type("Oliver")
-    cy.get('[data-cy=select-country-select-container-wrapper] > .nui-react-select-container > .nui-react-select__control > .nui-react-select__value-container > .nui-react-select__single-value').type('Pakistan{enter}{enter}');
-    cy.get('[data-cy=select-time-zone-select-container-wrapper] > .nui-react-select-container > .nui-react-select__control > .nui-react-select__value-container > .nui-react-select__single-value').type('America/Dawson-UTC-07.00{enter}{enter}');
+    cy.get('[data-cy=select-country-select-container-wrapper]').type('Canada');
+    cy.get('[data-cy=select-time-zone-select-container-wrapper]').type('America/Dawson-UTC-07.00');
     cy.get(':nth-child(2) > [data-cy=profile-date-format-select]').click();
     cy.get('[data-cy=profile-submit-button]').click();
     cy.get('[data-cy=toastr-message-container]').should('have.text',"Profile successfully updated!");  
